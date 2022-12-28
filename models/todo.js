a83171e3061afa9ef016cebf9cc0202304e9ff86
Pallavipriti <78161290+Pallavipriti@users.noreply.github.com>
2022-12-28 18:21:36 +0530

    Update todo.js

--- models/todo.js
+++ models/todo.js
@@ -1,5 +1,5 @@
 "use strict";
-const { Model } = require("sequelize");
+const { Model, Op } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Todo extends Model {
     /**
@@ -7,6 +7,8 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
+
+    // eslint-disable-next-line no-unused-vars
     static associate(models) {
       // define association here
     }
@@ -17,6 +19,89 @@ module.exports = (sequelize, DataTypes) => {
 
     markAsCompleted() {
       return this.update({ completed: true });
+    }
+
+    setCompletionStatus(status) {
+      return this.update({ completed: status });
+    }
+
+    static async getTodos() {
+      return await this.findAll();
+    }
+
+    async deleteTodo() {
+      await this.destroy();
+    }
+
+    static async overdue(includeCompleted = false) {
+      if (includeCompleted) {
+        return await Todo.findAll({
+          where: {
+            dueDate: {
+              [Op.lt]: new Date().toLocaleDateString("en-CA"),
+            },
+          },
+        });
+      } else {
+        return await Todo.findAll({
+          where: {
+            dueDate: {
+              [Op.lt]: new Date().toLocaleDateString("en-CA"),
+            },
+            completed: false,
+          },
+        });
+      }
+    }
+
+    static async dueToday(includeCompleted = false) {
+      if (includeCompleted) {
+        return await Todo.findAll({
+          where: {
+            dueDate: {
+              [Op.eq]: new Date().toLocaleDateString("en-CA"),
+            },
+          },
+        });
+      } else {
+        return await Todo.findAll({
+          where: {
+            dueDate: {
+              [Op.eq]: new Date().toLocaleDateString("en-CA"),
+            },
+            completed: false,
+          },
+        });
+      }
+    }
+
+    static async dueLater(includeCompleted = false) {
+      if (includeCompleted) {
+        return await Todo.findAll({
+          where: {
+            dueDate: {
+              [Op.gt]: new Date().toLocaleDateString("en-CA"),
+            },
+          },
+        });
+      } else {
+        return await Todo.findAll({
+          where: {
+            dueDate: {
+              [Op.gt]: new Date().toLocaleDateString("en-CA"),
+            },
+            completed: false,
+          },
+        });
+      }
+    }
+
+    static async completed() {
+      return await Todo.findAll({
+        where: {
+          completed: true,
+        },
+      });
     }
   }
   Todo.init(
